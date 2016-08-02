6794ff9ab034337cb5f4c1efca0261e41c85f649
Will Hogan <willhogan11@hotmail.com>
2016-08-03 00:06:51 +0100

    json data now separate, working on routing

--- js/controller.js
+++ js/controller.js
@@ -1,42 +1,7 @@
-var myApp = angular.module('myApp', []);
+var personController = angular.module('personController', []);
 
-myApp.controller("MyController", function MyController($scope){
-	$scope.persons =
-	[
-		{
-			"name": "Will Hogan",
-			"shortname": "will",
-			"age": 39,
-			"address": "Galway",
-			"favColour": "blue"
-		},
-		{
-			"name": "Gillian Hogan",
-			"shortname": "gill",
-			"age": 36,
-			"address": "Galway",
-			"favColour": "green"
-		},
-		{
-			"name": "Chris Hogan",
-			"shortname": "chris",
-			"age": 8,
-			"address": "Galway",
-			"favColour": "green"
-		},
-		{
-			"name": "Olivia Hogan",
-			"shortname": "olivia",
-			"age": 7,
-			"address": "Galway",
-			"favColour": "teal"
-		},
-		{
-			"name": "Ruairí Hogan",
-			"shortname": "ruairí",
-			"age": 3,
-			"address": "Galway",
-			"favColour": "grey"
-		}
-	]
-});
+personController.controller("ListController", ["$scope", "$http", function($scope, $http){
+	$http.get("js/person.json").success(function(data){
+		$scope.persons = data;
+	});
+}]);
