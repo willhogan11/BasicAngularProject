8e637f5374b0f57be43f0713dc02566f2add9c35
Will Hogan <willhogan11@hotmail.com>
2016-08-03 22:32:07 +0100

    Working on PersonDetails page routing

--- angular/js/app.js
+++ angular/js/app.js
@@ -1,7 +1,16 @@
-var myApp = angular.module('myApp', ['ngRoute', 'personController']);
+var myApp = angular.module("myApp", ["ngRoute", "personController"]);
 
-myApp.config(['$routeProvider', function($routeProvider){
+myApp.config(["$routeProvider", function($routeProvider){
 	$routeProvider.
-		when('/personlist', {templateUrl: 'partials/personlist.html', controller: 'ListController'}).
-		otherwise({redirectTo: '/personlist'});
+		when("/personlist", {
+			templateUrl: "partials/personlist.html",
+			controller: "ListController"
+		}).
+		when("/personDetails", {
+			templateUrl: "partials/personDetails.html",
+		controller: "DetailsController"
+		}).
+		otherwise({
+			redirectTo: "/personlist"
+		});
 }]);
--- angular/js/controller.js
+++ angular/js/controller.js
@@ -1,7 +1,14 @@
-var personController = angular.module('personController', []);
+var personController = angular.module("personController", []);
 
-personController.controller('ListController', ['$scope', '$http', function($scope, $http){
-	$http.get('js/person.json').success(function(data){
+personController.controller("ListController", ["$scope", "$http", function($scope, $http){
+	$http.get("js/person.json").success(function(data){
+		$scope.persons = data;
+	});
+}]);
+
+
+personController.controller("DetailsController", ["$scope", "$http", function($scope, $http){
+	$http.get("js/person.json").success(function(data){
 		$scope.persons = data;
 	});
 }]);
